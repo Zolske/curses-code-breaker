8089bc087f26ee0244e6b016b3317c4ca9a00b30
ZolsKe <zoltan.the.kepes@gmail.com>
2022-02-06 15:32:15 +0000

    heroku: moved curses.curs_set(0) into try block

--- run.py
+++ run.py
@@ -16,7 +16,11 @@ screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
 screen.keypad(True)
-curses.curs_set(0)  # make cursor invisible
+try:
+    curses.curs_set(0)  # make cursor invisible
+except:
+    pass
+
 # color pairs used by 'curses' and accessible over variable below, 1st text, 2nd background
 curses.start_color()
 curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_RED)
